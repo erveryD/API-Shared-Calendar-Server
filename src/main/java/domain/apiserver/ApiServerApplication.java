7d2d51c67a24812e6f7847e7fcfd522ad7d33fa5
yeonghoon.lee <djaakclsrn12@naver.com>
2023-09-23 23:35:32 +0900

    NEW: ConfigurationPropertiesScan 어노테이션 추가

--- src/main/java/domain/apiserver/ApiServerApplication.java
+++ src/main/java/domain/apiserver/ApiServerApplication.java
@@ -2,8 +2,12 @@ package domain.apiserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class})
+@ConfigurationPropertiesScan(value = {"domain.apiserver"})
 public class ApiServerApplication {
 
 	public static void main(String[] args) {
