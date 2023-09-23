54922c4ee22a7627c4ab34993a70a3f375bf99d9
yeonghoon.lee <djaakclsrn12@naver.com>
2023-09-23 23:29:41 +0900

    TST: 기본 테스트 주석처리

--- src/test/java/domain/apiserver/ApiServerApplicationTests.java
+++ src/test/java/domain/apiserver/ApiServerApplicationTests.java
@@ -2,12 +2,12 @@ package domain.apiserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class ApiServerApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//
+//@SpringBootTest
+//class ApiServerApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
