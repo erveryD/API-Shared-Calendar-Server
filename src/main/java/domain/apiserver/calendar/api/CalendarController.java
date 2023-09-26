36a30b88c3452e869f03706d48e9130b42c05941
2Yeonghoon <djaakclsrn12@naver.com>
2023-09-26 22:57:20 +0900

    NEW: 캘린더 컨트롤러 추가

---
+++ src/main/java/domain/apiserver/calendar/api/CalendarController.java
@@ -0,0 +1,12 @@
+package domain.apiserver.calendar.api;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "구독")
+@RestController
+@RequiredArgsConstructor
+public class CalendarController {
+
+}
